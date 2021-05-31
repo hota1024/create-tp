f403b93c8eed3743da2aa61eef8f17fd90c8c63c
hota1024 <hotalog@hota1024.com>
2021-05-31 22:37:19 +0900

    ✨ add reactlib

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ class CreateTp extends Command {
     code: flags.boolean({ char: 'c' }),
     template: flags.string({
       char: 't',
-      options: ['project', 'lib', 'next', 'next-material'],
+      options: ['project', 'lib', 'reactlib', 'next', 'next-material'],
     }),
     repo: flags.string({ char: 'r' }),
   }
